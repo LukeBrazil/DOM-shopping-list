4cb0a3f6ceb5f9ab879ee8844b7451a337fc5b14
Luke Brazil <Luke.brazil13@icloud.com>
2020-08-19 11:15:49 -0500

    Initial Commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 'use strict';
+const myBtn = document.querySelector('#generateList');
+const shopList = document.querySelector('#shopping-list');
+
+
+myBtn.addEventListener('click', function() {
+const item1 = document.querySelector('.input1').value;
+const item2 = document.querySelector('.input2').value;
+const item3 = document.querySelector('.input3').value;
+const item4 = document.querySelector('.input4').value;
+    shopList.innerHTML = `
+<div class="container">
+    <h2>Shopping List:</h2>
+    <br>
+    <li>${item1}</li>
+    <li>${item2}</li>
+    <li>${item3}</li>
+    <li>${item4}</li>
+</div>
+    `
+});
